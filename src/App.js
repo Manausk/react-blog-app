f09fc8b46637306c1e6286e26d00e09c5872a4e5
ManausK <1905246@kiit.ac.in>
2023-06-08 06:52:25 +0530

    user settings component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import TopBar from "./components/topbar/TopBar";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 function App() {
   return (
     <div className="App">
       <>
         <TopBar />
-        <Write />
+        <Settings />
       </>
     </div>
   );
