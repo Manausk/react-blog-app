5ecd906a9d9cecb9662d7c9fee639b1fc4f8e738
ManausK <1905246@kiit.ac.in>
2023-06-08 07:25:19 +0530

    React login and register page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import TopBar from "./components/topbar/TopBar";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
 // import Write from "./pages/write/Write";
-import Settings from "./pages/settings/Settings";
+// import Settings from "./pages/settings/Settings";
+// import Login from "./components/posts/login/Login";
+import Register from "./components/posts/register/Register";
 function App() {
   return (
     <div className="App">
       <>
         <TopBar />
-        <Settings />
+        <Register />
       </>
     </div>
   );
